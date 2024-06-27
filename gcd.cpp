bf2a0806fef57605e51b49cdaa04d053e38114cd
Ayush Jangid <94820278+ayushjangid1@users.noreply.github.com>
2024-06-27 21:53:04 +0530

    goodNearlyGood

---
+++ gcd.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int gcd(int a,int b){
+    if(a==0){
+        return b;
+    }
+    return gcd(b%a,a);
+}
+
+int main()
+{
+
+    int a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b)<<endl;
+
+    return 0;
+}
