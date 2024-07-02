2835a61fa4db84c091dd444fd8afeeb86573e578
Ayush Jangid <94820278+ayushjangid1@users.noreply.github.com>
2024-07-02 13:34:13 +0530

    k

---
+++ increasing_arr.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n,0);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int ans =0;
+        for(int i=1;i<n;i++){
+            if(arr[i] >= arr[i-1]){
+                continue;
+            }
+            else{
+                ans+=(arr[i-1] - arr[i]);
+                arr[i] = arr[i-1];
+            }
+        }
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
